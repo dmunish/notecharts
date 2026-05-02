6a97bf62bc48adf681ac7dfdab0f814a6993d7bc
dmunish <thenish2004@gmail.com>
2026-05-02 07:47:10 +0500

    import new interface

--- notecharts/__init__.py
+++ notecharts/__init__.py
@@ -1,4 +1,5 @@
-from .widget import EChartsWidget
+from .widget import Chart, JSCode
+from .charts import Bar, Line, Scatter
 
 __version__ = "0.1.0"
-__all__ = ["EChartsWidget"]
+__all__ = ["Chart", "JSCode", "Bar", "Line", "Scatter"]
