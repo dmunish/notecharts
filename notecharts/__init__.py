fca01c8c5adad4835e19beab99412b99961961d7
dmunish <thenish2004@gmail.com>
2026-05-02 19:08:06 +0500

    Import the new primitives

--- notecharts/__init__.py
+++ notecharts/__init__.py
@@ -1,5 +1,5 @@
 from .widget import Chart, JSCode
-from .charts import Bar, Line, Scatter
+from .charts import Bar, Line, Scatter, Donut, Radar
 
 __version__ = "0.1.0"
-__all__ = ["Chart", "JSCode", "Bar", "Line", "Scatter"]
+__all__ = ["Chart", "JSCode", "Bar", "Line", "Scatter", "Donut", "Radar"]
