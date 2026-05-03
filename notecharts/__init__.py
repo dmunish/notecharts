565311153794f8ac60707e21a72d04c0067db374
dmunish <thenish2004@gmail.com>
2026-05-03 22:54:47 +0500

    Remove Donut

--- notecharts/__init__.py
+++ notecharts/__init__.py
@@ -1,5 +1,5 @@
 from .widget import Chart, JSCode
-from .charts import Bar, Line, Scatter, Donut, Radar
+from .charts import Bar, Line, Scatter, Radar
 
 __version__ = "0.1.0"
 __all__ = ["Chart", "JSCode", "Bar", "Line", "Scatter", "Radar"]
