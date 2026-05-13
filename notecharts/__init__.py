cec91381f66b0d21824624ed51ffa5a4c4ae995e
dmunish <thenish2004@gmail.com>
2026-05-13 10:31:04 +0500

    Add Palette function and version

--- notecharts/__init__.py
+++ notecharts/__init__.py
@@ -1,5 +1,6 @@
 from .widget import Chart, JSCode
 from .charts import Bar, Line, Scatter, Radar
+from .palette import Palette
 
-__version__ = "0.1.0"
-__all__ = ["Chart", "JSCode", "Bar", "Line", "Scatter", "Radar"]
+__version__ = "0.3.0"
+__all__ = ["Chart", "JSCode", "Bar", "Line", "Scatter", "Radar", "Palette"]
