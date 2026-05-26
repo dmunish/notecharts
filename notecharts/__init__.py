9e09870333ef282824e920645fc5fb732104ac1f
dmunish <thenish2004@gmail.com>
2026-05-26 10:49:15 +0500

    [chore] bump version number

--- notecharts/__init__.py
+++ notecharts/__init__.py
@@ -2,5 +2,5 @@ from .widget import Chart, JSCode
 from .charts import Bar, Line, Scatter, Radar
 from .palette import Palette
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = ["Chart", "JSCode", "Bar", "Line", "Scatter", "Radar", "Palette"]
