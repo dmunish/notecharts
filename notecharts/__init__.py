2470116ab6e8443ed04002640132b49f143c829a
dmunish <thenish2004@gmail.com>
2026-06-06 16:55:45 +0500

    Import templates from the new split files

--- notecharts/__init__.py
+++ notecharts/__init__.py
@@ -1,5 +1,8 @@
 from .widget import Chart, JSCode
-from .charts import Bar, Line, Scatter, Radar
+from .templates.bar import Bar
+from .templates.line import Line
+from .templates.radar import Radar
+from .templates.scatter import Scatter
 from .palette import Palette
 
 __version__ = "0.4.0"
